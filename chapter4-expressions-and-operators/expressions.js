dbf83bc745089110d896fc4c1e0254371fcdec63
boodscode237 <boodscode@gmail.com>
2022-11-20 10:17:38 +0500

    expressions finish

---
+++ chapter4-expressions-and-operators/expressions.js
@@ -0,0 +1,17 @@
+a = -5
+b = {'a': 4}
+console.log(1 + b['a'])
+console.log(false + true * 2)
+let i = 1, j = ++i;
+let n = 1, m = n++;
+console.log(i, j, n, m)
+
+console.log(0xFF00 ^ 0xF0F0)
+console.log(0 === -0)
+
+имя = 'ДОНАЛЬД'
+console.log(имя)
+
+d = 8
+
+console.log(d)
